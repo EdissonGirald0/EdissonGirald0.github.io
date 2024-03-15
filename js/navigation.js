869f572a6b1ee0d15dacb1d9eba96d5a37dcd1ef
Edisson Giraldo <edisson.giraldo@hotmail.com>
2024-03-15 09:04:35 -0500

    Algunas mejoras de usabilidad

--- js/navigation.js
+++ js/navigation.js
@@ -1,16 +1,26 @@
-// navigation.js
 document.addEventListener("DOMContentLoaded", function () {
     var navLinks = document.querySelectorAll('header nav a, footer nav a');
     navLinks.forEach(function (link) {
         link.addEventListener('click', function (event) {
-            event.preventDefault();
-            var current = document.getElementsByClassName('selected');
-            current[0].className = current[0].className.replace(' selected', '');
-            this.className += ' selected';
-            var target = this.getAttribute('href').substring(1);
-            document.getElementById(target).scrollIntoView({
-                behavior: 'smooth'
-            });
+            // Verificar si el enlace es interno o externo
+            var href = this.getAttribute('href');
+            if (href && href.charAt(0) === '#') {
+                // Enlace interno: desplazamiento suave
+                event.preventDefault();
+                var target = href.substring(1);
+                document.getElementById(target).scrollIntoView({
+                    behavior: 'smooth'
+                });
+                // Agregar la clase 'selected' al enlace activo
+                var current = document.getElementsByClassName('selected');
+                if (current.length > 0) {
+                    current[0].classList.remove('selected');
+                }
+                this.classList.add('selected');
+            } else {
+                // Enlace externo: abrir en una nueva pestaña
+                this.setAttribute('target', '_blank');
+            }
         });
     });
 });
